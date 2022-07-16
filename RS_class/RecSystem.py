0cd30bef0e3ab693d2570d4f1812adcbd111ce94
Pedro Alves <pedroalves0409@gmail.com>
2022-07-16 17:33:01 -0300

    class_commit

--- RS_class/RecSystem.py
+++ RS_class/RecSystem.py
@@ -6,18 +6,23 @@ from math import sqrt
 
 class KnnMv():
 
+    def __init__(self, filmes_path, notas_path):
+        self.filmes_path = filmes_path
+        self.notas_path = notas_path
+
+
     # função que pega os dados e renomeia colunas
-    def get_data(filmes_path, notas_path):
+    def get_data(self):
         # lendo os dados
-        filmes = pd.read_csv(filmes_path)
-        notas = pd.read_csv(notas_path)
+        filmes = pd.read_csv(self.filmes_path)
+        notas = pd.read_csv(self.notas_path)
 
         # alterando os nomes das colunas
         filmes.columns = ['filmeId', 'titulo', 'generos']
         notas.columns = ['usuarioId', 'filmeId', 'nota', 'momento']
         return filmes, notas
 
-    def aux_df(notas, filmes):
+    def aux_df(self, notas, filmes):
         # criando tabela única para melhor análise
         df = pd.merge(notas, filmes, how = 'left', right_on = 'filmeId', left_on = 'filmeId')
         # criando coluna com a quantidade de notas atribuídas aos filmes
@@ -43,17 +48,17 @@ class KnnMv():
         return dist
 
     # função que retorna dataframe com os coluna de notas e index como o id do filme
-    def notas_do_usuario(usuario):
-
+    def notas_do_usuario(self, data_frame, usuario):
         # fltra o dataframe pelo usuarioId
-        notas_do_usuario = df[df['usuarioId'] == usuario]
+        notas_do_usuario = data_frame[data_frame['usuarioId'] == usuario]
         
         # setta o index como a coluna de filmeId
         notas_do_usuario = notas_do_usuario[['filmeId', 'nota']].set_index(['filmeId'])
         return notas_do_usuario
 
-    def distancia_de_usuarios(usuario_id1, usuario_id2):
-
+    def distancia_de_usuarios(self, usuario_id1, usuario_id2):
+        self.notas_do_usuario()
+        self.distancia_de_vetores()
         # trás notas dos usuários 1 e 2
         notas1 = notas_do_usuario(usuario_id1)
         notas2 = notas_do_usuario(usuario_id2)
@@ -66,15 +71,17 @@ class KnnMv():
         return [usuario_id1, usuario_id2, distancia]
 
 
-    def distancia_de_todos(voce_id):
-
+    def distancia_de_todos(self, voce_id):
+        self.notas
+        self.distancia_de_usuarios()
         todos_os_usuarios = notas['usuarioId'].unique()
         distancias = [distancia_de_usuarios(voce_id, usuario_id) for usuario_id in todos_os_usuarios]
         distancias = pd.DataFrame(distancias, columns = ['voce', 'outra_pessoa', 'distancia'])
         return distancias
 
-    def distancia_de_usuarios(usuario_id1, usuario_id2, minimo = 5):
-
+    def distancia_de_usuarios(self, usuario_id1, usuario_id2, minimo = 5):
+        self.notas_do_usuario()
+        self.distancia_de_vetores()
         # trás notas dos usuários 1 e 2
         notas1 = notas_do_usuario(usuario_id1)
         notas2 = notas_do_usuario(usuario_id2)
@@ -89,14 +96,17 @@ class KnnMv():
         distancia = distancia_de_vetores(diferencas['nota_esquerda'], diferencas['nota_direita'])
         return [usuario_id1, usuario_id2, distancia]
 
-    def mais_proximos_de(voce_id, n_mais_proximos = 5, numero_de_usuarios_a_analisar = None):
+    def mais_proximos_de(self, voce_id, n_mais_proximos = 5, numero_de_usuarios_a_analisar = None):
+        self.distancia_de_todos()
         distancias = distancia_de_todos(voce_id, numero_de_usuarios_a_analisar = numero_de_usuarios_a_analisar)
         mp = distancias.sort_values(by = 'distancia')
         mp = distancias.set_index('outra_pessoa').drop(voce_id, errors = 'ignore')
         
         return mp
 
-    def distancia_de_todos(voce_id, numero_de_usuarios_a_analisar = None):
+    def distancia_de_todos(self, voce_id, numero_de_usuarios_a_analisar = None):
+        self.notas
+        self.distancia_de_usuarios()
         todos_os_usuarios = notas['usuarioId'].unique()
         if numero_de_usuarios_a_analisar:
             todos_os_usuarios = todos_os_usuarios[:numero_de_usuarios_a_analisar]
@@ -105,7 +115,9 @@ class KnnMv():
         distancias = pd.DataFrame(distancias, columns = ['voce', 'outra_pessoa', 'distancia'])
         return distancias
 
-    def sugere_para(voce, n_mais_proximos = 10, numero_de_usuarios_a_analisar = None):
+    def sugere_para(self, voce, n_mais_proximos = 10, numero_de_usuarios_a_analisar = None):
+        self.notas_do_usuario()
+        self.mais_proximos_de()
         # suas notas
         notas_de_voce = notas_do_usuario(voce)
 
